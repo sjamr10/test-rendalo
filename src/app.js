1b47113bccf864c3333875ec0d53444004b53a89
Sergio Miranda <smiranda@xsdev.tech>
2019-11-04 20:45:27 -0300

    Add koa-bodyparser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const logger = require("koa-logger");
+const bodyParser = require("koa-bodyparser");
 
 const app = new Koa();
 const router = new Router();
@@ -9,6 +10,7 @@ const routes = require("./routes");
 routes({ router });
 
 app.use(logger());
+app.use(bodyParser());
 
 app.use(router.routes());
 app.use(router.allowedMethods());
