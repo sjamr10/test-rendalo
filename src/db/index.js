79f98ff014b25885c4ccf1fbf06c52ca66e36882
Sergio Miranda <smiranda@xsdev.tech>
2019-11-04 20:42:20 -0300

    Add Person schema

--- src/db/index.js
+++ src/db/index.js
@@ -4,3 +4,36 @@ mongoose.connect("mongodb://mongo/my_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const PersonSchema = new Schema({
+  id: ObjectId,
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+});
+
+const Person = mongoose.model("Person", PersonSchema);
+
+const getPeople = async () => {
+  const result = await Person.find().exec();
+  return result;
+};
+
+const savePerson = async newPerson => {
+  const person = new Person(newPerson);
+  const result = await person.save();
+  return result;
+};
+
+module.exports = {
+  getPeople,
+  savePerson
+};
