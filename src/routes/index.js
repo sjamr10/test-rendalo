bce420f71189d8e82cdfaf248b9ca1235352b728
Sergio Miranda <smiranda@xsdev.tech>
2019-11-04 20:49:05 -0300

    Add db code to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,22 @@ const db = require("../db");
 
 module.exports = ({ router }) => {
   router.get("/api/people", async (ctx, next) => {
-    ctx.body = {};
+    try {
+      const result = await db.getPeople();
+      ctx.body = result;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = error;
+    }
   });
 
   router.post("/api/people", async (ctx, next) => {
-    ctx.body = {};
+    try {
+      const result = await db.savePerson(ctx.request.body);
+      ctx.body = result;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = error;
+    }
   });
 };
