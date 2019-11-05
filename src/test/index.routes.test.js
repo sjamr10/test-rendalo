9d7f9d707aafdb9e6234d1ba74c62bc418090750
Sergio Miranda <smiranda@xsdev.tech>
2019-11-04 22:25:17 -0300

    Add test code

--- src/test/index.routes.test.js
+++ src/test/index.routes.test.js
@@ -6,9 +6,29 @@ afterEach(() => {
 });
 
 describe("routes: /api/people", () => {
-  test("should respond as expected", async () => {
+  test("GET - should respond as expected", async () => {
     const response = await request(server).get("/api/people");
     expect(response.status).toEqual(200);
     expect(response.type).toEqual("application/json");
+    expect(response.body.length).toBeDefined();
+  });
+
+  test("POST - should respond as expected", async () => {
+    let response = await request(server)
+      .post("/api/people")
+      .send({ name: "Sergio", age: 29 })
+      .set("Accept", "application/json");
+
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("age");
+
+    response = await request(server)
+      .post("/api/people")
+      .send({ name: "Sergio" })
+      .set("Accept", "application/json");
+
+    expect(response.status).toEqual(500);
   });
 });
